bbea4c62b4b7c14f650542d70866da14a4f1698f
gosta vo <dwwww@gos.tux.com>
2025-09-27 12:11:43 +0700

    Short, clear description of change20

--- routes/web.php
+++ routes/web.php
@@ -83,8 +83,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 });
 
-Route::get('/lang/{locale}', [LanguageController::class, 'switch'])->name('lang.switch');
+
+
 
 require __DIR__.'/auth.php';
